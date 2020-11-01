a6fbfef975c0f5c6d7daed22e0da69b33be97349
Ivo Smid <ivo.smid@gmail.com>
2020-11-01 13:23:32 +0100

    Initial commit

---
+++ src/test/java/cz/bedla/XxxTest.java
@@ -0,0 +1,11 @@
+package cz.bedla;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class XxxTestTest {
+    @Test
+    public void name() {
+        Assert.assertEquals("bar", System.getProperty("FOO"));
+    }
+}
